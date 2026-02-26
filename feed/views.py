cc2eff031298f10c9a96f5b467b07ccf9bde0048
Detravious Jamari Brinkley <dj.brinkley@ufl.edu>
2026-02-25 21:25:57 -0500

    feat: using django render for html pages within feed app

--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 # when a user to to localhost:8000/home
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>') 
+    # return HttpResponse('<h1>Blog Home</h1>') 
+    return render(request, 'feed/home.html')
+
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    # return HttpResponse('<h1>About</h1>')
+    return render(request, 'feed/about.html')
