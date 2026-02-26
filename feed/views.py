81d96c8e19f4febe9ad2080064f2f98b9547a2ee
Detravious Jamari Brinkley <dj.brinkley@ufl.edu>
2026-02-26 08:44:35 -0500

    feat: adding content + load static css + base html to extend

--- feed/views.py
+++ feed/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-# Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 # when a user to to localhost:8000/home
 def home(request):
-    # return HttpResponse('<h1>Blog Home</h1>') 
-    return render(request, 'feed/home.html')
+    return render(request, 
+                  'feed/home.html', 
+                  context = {'posts': posts}
+                  )
 
 
 def about(request):
     # return HttpResponse('<h1>About</h1>')
-    return render(request, 'feed/about.html')
+    return render(request, 'feed/about.html', {'title': 'About'})
